ef514ef475e53b6e55dac053bb8855a532e06f1d
Herinson Rodrigues <herinson.br@gmail.com>
2019-10-18 11:08:42 -0300

    Remove sc2renderer from bot source

--- src/sc2_bot.cpp
+++ src/sc2_bot.cpp
@@ -1,7 +1,6 @@
 #include <sc2api/sc2_api.h>
 #include <sc2utils/sc2_manage_process.h>
 #include <sc2renderer/sc2_renderer.h>
-
 #include <iostream>
 
 using namespace sc2;
@@ -11,52 +10,13 @@ const int kFeatureLayerSize = 80;
 const int kPixelDrawSize = 5;
 const int kDrawSize = kFeatureLayerSize * kPixelDrawSize;
 
-void DrawFeatureLayer1BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
-    assert(image_data.bits_per_pixel() == 1);
-    int width = image_data.size().x();
-    int height = image_data.size().y();
-    renderer::Matrix1BPP(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
-}
-
-void DrawFeatureLayerUnits8BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
-    assert(image_data.bits_per_pixel() == 8);
-    int width = image_data.size().x();
-    int height = image_data.size().y();
-    renderer::Matrix8BPPPlayers(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
-}
-
-void DrawFeatureLayerHeightMap8BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
-    assert(image_data.bits_per_pixel() == 8);
-    int width = image_data.size().x();
-    int height = image_data.size().y();
-    renderer::Matrix8BPPHeightMap(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
-}
-
 class SimpleBot : public Agent {
 public:
-    virtual void OnGameStart() final {
-        renderer::Initialize("Feature layers", 50, 50, 2 * kDrawSize, 2 * kDrawSize);
-    }
+	virtual void OnStep() final {
+		std::cout << Observation()->GetGameLoop() << std::endl;
+	}
 
-    virtual void OnStep() final {
-        const SC2APIProtocol::Observation* observation = Observation()->GetRawObservation();
-
-        const SC2APIProtocol::FeatureLayers& m = observation->feature_layer_data().renders();
-        DrawFeatureLayerUnits8BPP(m.unit_density(), 0, 0);
-        DrawFeatureLayer1BPP(m.selected(), kDrawSize, 0);
-
-        const SC2APIProtocol::FeatureLayersMinimap& mi = observation->feature_layer_data().minimap_renders();
-        DrawFeatureLayerHeightMap8BPP(mi.height_map(), 0, kDrawSize);
-        DrawFeatureLayer1BPP(mi.camera(), kDrawSize, kDrawSize);
-
-        renderer::Render();
-    }
-
-    virtual void OnGameEnd() final {
-        renderer::Shutdown();
-    }
-
-	virtual void OnUnitIdle(const Unit* unit) final {
+    virtual void OnUnitIdle(const Unit* unit) final {
 		switch (unit->unit_type.ToType()) {
 			case UNIT_TYPEID::TERRAN_COMMANDCENTER: {
 				Actions()->UnitCommand(unit, ABILITY_ID::TRAIN_SCV);
@@ -121,9 +81,6 @@ int main(int argc, char* argv[]) {
     if (!coordinator.LoadSettings(argc, argv)) {
         return 1;
     }
-
-    FeatureLayerSettings settings(kCameraWidth, kFeatureLayerSize, kFeatureLayerSize, kFeatureLayerSize, kFeatureLayerSize);
-    coordinator.SetFeatureLayers(settings);
 
     // Add the custom bot, it will control the players.
     SimpleBot bot;
